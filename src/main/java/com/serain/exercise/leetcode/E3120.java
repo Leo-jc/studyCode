a14bd3e512955dea7fce6eb38e9b0ab6a9fe4f3e
jianchuanZeng <jianchuanz@foxmail.com>
2026-05-26 10:51:08 +0800

    feat(leetcode): 添加跳跃游戏可达性判断算法实现
    - 实现了基于动态规划和前缀和的跳跃可达性判断方法
    - 使用 dp 数组记录每个位置是否可达的状态
    - 通过 preSum 前缀和数组优化区间查询性能
    - 处理最小跳跃距离和最大跳跃距离的约束条件
    - 验证目标位置是否为障碍物并返回相应结果
    - 优化时间复杂度为 O(n) 的高效解决方案

---
+++ src/main/java/com/serain/exercise/leetcode/E3120.java
@@ -0,0 +1,33 @@
+package com.serain.exercise.leetcode;
+
+/**
+ * @BelongsProject: studyCode
+ * @BelongsPackage: com.serain.exercise.leetcode
+ * @Author: Serain
+ * @CreateTime: 2026-05-26  10:42
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public class E3120 {
+    public int numberOfSpecialChars(String word) {
+        int count = 0;
+        int n=word.length();
+        boolean[] isSmall = new boolean[26];
+        boolean[] isCapital = new boolean[26];
+        for(int i=0;i<n;i++){
+            char c = word.charAt(i);
+            if(c>='a' && c<='z'){
+                isSmall[c-'a'] = true;
+            }
+            if(c>='A' && c<='Z'){
+                isCapital[c-'A'] = true;
+            }
+        }
+        for(int i=0;i<26;i++){
+            if(isSmall[i] && isCapital[i]){
+                count++;
+            }
+        }
+        return count;
+    }
+}
